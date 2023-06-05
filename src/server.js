19c2dc1ca9a7baa6399b723527d45e9d8bf3e5b9
roshan <roshanprusty100@gmail.com>
2023-06-05 10:43:38 +0530

    -

--- src/server.js
+++ src/server.js
@@ -7,8 +7,24 @@ const httpServer = createServer(app);
 const io = new Server(httpServer);
 const handleSocketevents = require("./ws/socket");
 const port = process.env.PORT || 3000;
+const mongoose= require("mongoose");
+const bodyParser= require("body-parser");
 
 app.use(express.static(path.join(__dirname, "public")));
+
+mongoose.connect("",{useNewUrlParser:true},{useUnifiedTopology:true})
+.then(()=>{
+  console.log("Database connection done")
+}).catch(()=>{
+  console.log("something went wrong")
+})
+//creating a data schemea
+const notesSchema={
+  title:String,
+  content:String
+}
+const Note=mongoose.model("Note",notesSchema);
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
@@ -20,6 +36,14 @@ app.get("/join", (req, res) => {
   res.render("rtc.ejs");
 });
 
+app.post("/login", function(req,res){
+  let newNote=new Note({
+    room:req.body.room,
+    name:req.body.username
+  });
+  newNote.save();
+  res.redirect('/login');
+})
 httpServer.listen(port, () => {
   console.log("Server started at port " + port);
 });
